183afa634d76b8163a69eb6e2e86301f85aec78d
mohamed20911 <91696153+mohamed20911@users.noreply.github.com>
2021-10-02 16:10:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace project_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Enter Your name: ");
+            string TheName = Console.ReadLine();
+
+            Console.WriteLine("Welcome back " + TheName);
+
+        }
+    }
+}
